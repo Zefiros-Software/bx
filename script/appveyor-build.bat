9108a0ab30b82945024d9f2f8fe9fa15d83acb34
PaulVisscher <paul.pev@hotmail.com>
2018-02-06 19:51:27 +0100

    Update to zpm 2

--- script/appveyor-build.bat
+++
@@ -1,7 +0,0 @@
-cd test/
-zpm install-package --allow-install --allow-module  || exit /b 1
-zpm vs2015 || exit /b 1
-
-msbuild bx.sln /property:Configuration=Test /property:Platform=Win32 || exit /b 1
-
-.\bin\Test\bx.exe --help || exit /b 1
